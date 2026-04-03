26bcd4ff9500b40f75c81ea2974fd231635f6e40
Francesco Cicconardi <francicco@gmail.com>
2026-04-03 18:17:22 +0100

    Update __init__.py

--- extension/__init__.py
+++ extension/__init__.py
@@ -1,1 +1,19 @@
 
+from .filtering import filter_fasta_by_length, rename_repeatmodeler_headers
+from .extract_align import run_extract_align
+from .consensus import (
+    run_extend_consensus,
+    postprocess_extension_outputs,
+    categorize_extension,
+    ExtensionResult,
+)
+
+__all__ = [
+    "filter_fasta_by_length",
+    "rename_repeatmodeler_headers",
+    "run_extract_align",
+    "run_extend_consensus",
+    "postprocess_extension_outputs",
+    "categorize_extension",
+    "ExtensionResult",
+]
