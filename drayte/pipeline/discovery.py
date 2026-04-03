5d6102568929c69923ab22b6a1eaba4b427960c1
Francesco Cicconardi <francicco@gmail.com>
2026-04-03 14:52:27 +0100

    Update discovery.py

--- drayte/pipeline/discovery.py
+++ drayte/pipeline/discovery.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from drayte.utils.paths import stage_dir
 from drayte.utils.subprocess import run_command
 
@@ -12,14 +10,14 @@ def run(config, logger) -> dict:
     cmd = [
         "python",
         config.tools.step1_repmodannotation,
-        "--genome",
-        str(config.genome_path.resolve()),
-        "--species",
-        config.species,
-        "--threads",
-        str(config.threads),
-        "--outdir",
-        str(outdir),
+        "--genome", str(config.genome_path.resolve()),
+        "--outdir", str(outdir),
+        "--species", config.species,
+        "--threads", str(config.threads),
+        "--batches", str(config.extra.get("batches", 1)),
+        "--repeatmodeler-dir", config.extra["repeatmodeler_dir"],
+        "--repeatscout-dir", config.extra["repeatscout_dir"],
+        "--repeatmasker-bin", config.extra.get("repeatmasker_bin", "RepeatMasker"),
     ]
 
     run_command(cmd, logger)
@@ -28,7 +26,9 @@ def run(config, logger) -> dict:
         "stage": "discovery",
         "outdir": str(outdir),
         "genome": str(config.genome_path.resolve()),
-        "raw_library": str(outdir / "consensi.fa.classified"),
+        "raw_library": str(outdir / "rmodeler_dir" / "consensi.fa.classified"),
+        "normalized_library": str(outdir / "rmodeler_dir" / f"{config.species}-families.mod.fa"),
+        "repeatmasker_dir": str(outdir / "rmasker_dir"),
     }
 
     logger.info("Discovery stage completed")
